8852957d1e595ea10e66dc8df4421f3cafe4e505
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-14 09:02:09 -0500

    feat: simplificar componente NavBarCustom y ajustar propiedades

--- src/components/app/nav-bar-custom.tsx
+++ src/components/app/nav-bar-custom.tsx
@@ -3,44 +3,40 @@ import { SheetMenu } from './sheet-menu'
 import { useSidebar } from '@/hooks'
 import { cn } from '@/lib/utils'
 import { IMoreApp, SectionElement } from '@/types'
+import { BreadcrumbCustom } from './bread-crumb-custom'
 import { useStore } from 'zustand'
-import { MENU_PROFILE } from '@/config/profile-menu'
-import { ProfilePopover } from './profile-popover'
-import { BreadcrumbCustom } from '../panel-admin/bread-crumb-custom'
+// import { MENU_PROFILE } from '@/config/profile-menu'
+// import { ProfilePopover } from './profile-popover'
 // import { SidebarToggle } from './sidebar-toggle'
 // import { Grip } from 'lucide-react'
 // import { Breadcrumb } from '@/components/ui/breadcrumb'
 
 interface NavBarCustomProps {
-  app?: keyof typeof configApps
   moreApps?: Array<IMoreApp>
   email?: string
   menuItems?: SectionElement[]
 }
 
 export const NavBarCustom = (props: NavBarCustomProps) => {
-  const { app, email, menuItems } = props
+  const { menuItems } = props
 
   const sidebar = useStore(useSidebar, (x) => x)
   if (!sidebar) return null
   // const { isOpen, toggleOpen } = sidebar
 
-  const appConfig = app ? configApps[app] : configApps['student']
-
-  const colorApp = appConfig.color
-  const nameApp = appConfig.name
-
   return (
     <header
       className={cn(`sticky top-0 z-50 w-full  shadow `)}
-      style={{
-        backgroundColor: colorApp,
-      }}
+      style={
+        {
+          // backgroundColor: colorApp,
+        }
+      }
     >
       <div className="px-4 sm:px-6 md:px-7 flex h-16 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0 sm:gap-3">
           <SheetMenu
-            title={nameApp}
+            title={'Service Request Manager'}
             menuItems={menuItems}
           />
           {/* <SidebarToggle
@@ -51,7 +47,7 @@ export const NavBarCustom = (props: NavBarCustomProps) => {
         </div>
         <div className="flex flex-1 items-center justify-end gap-2">
           {/*Menu de perfil*/}
-          <ProfilePopover
+          {/* <ProfilePopover
             profileData={{
               names: `${person?.names} ${person?.last_name1} ${person?.last_name2}`,
               email,
@@ -60,7 +56,7 @@ export const NavBarCustom = (props: NavBarCustomProps) => {
             menuSections={MENU_PROFILE.APP_MENU}
             showProgress={false}
             showBorders={false}
-          />
+          /> */}
         </div>
       </div>
     </header>
