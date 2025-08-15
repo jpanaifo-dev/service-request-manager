d432e1a41f42730ac8a82b6d6651314db9deb071
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-15 09:25:56 -0500

    feat: simplificar métodos de API eliminando el parámetro de módulo

--- src/api/core/methods.ts
+++ src/api/core/methods.ts
@@ -1,63 +1,45 @@
-import { ServicesModulesType } from '@/config/modules.cofig'
+// import { ServicesModulesType } from '@/config/modules.cofig'
 import { headerClient } from './header-client'
 
-export async function get(path: string, module: keyof ServicesModulesType) {
-  return headerClient(path, { method: 'GET' }, undefined, module)
+export async function get(path: string) {
+  return headerClient(path, { method: 'GET' }, undefined)
 }
 
-export async function post(
-  path: string,
-  body: unknown,
-  isFormData = false,
-  module: keyof ServicesModulesType
-) {
+export async function post(path: string, body: unknown, isFormData = false) {
   return headerClient(
     path,
     {
       method: 'POST',
       body: isFormData ? (body as FormData) : JSON.stringify(body),
     },
-    isFormData,
-    module
+    isFormData
   )
 }
 
-export async function put(
-  path: string,
-  body: unknown,
-  isFormData = false,
-  module: keyof ServicesModulesType
-) {
+export async function put(path: string, body: unknown, isFormData = false) {
   return headerClient(
     path,
     {
       method: 'PUT',
       body: isFormData ? (body as FormData) : JSON.stringify(body),
     },
-    isFormData,
-    module
+    isFormData
   )
 }
 
-export async function patch(
-  path: string,
-  body: unknown,
-  isFormData = false,
-  module: keyof ServicesModulesType
-) {
+export async function patch(path: string, body: unknown, isFormData = false) {
   return headerClient(
     path,
     {
       method: 'PATCH',
       body: isFormData ? (body as FormData) : JSON.stringify(body),
     },
-    isFormData,
-    module
+    isFormData
   )
 }
 
-export async function del(path: string, module: keyof ServicesModulesType) {
-  return headerClient(path, { method: 'DELETE' }, undefined, module)
+export async function del(path: string) {
+  return headerClient(path, { method: 'DELETE' }, undefined)
 }
 
 export const fetchMethods = {
