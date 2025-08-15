6c5ad71adf411237fce8811eaad9cada9e301d5b
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-15 09:10:33 -0500

    feat: agregar configuración de imágenes y variables de entorno en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'enlinea.unapiquitos.edu.pe',
+      },
+      {
+        protocol: 'https',
+        hostname: 'diariolaregion.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'http://192.168.16.182:8000/',
+      },
+    ],
+  },
+  env: {
+    APP_NAME: process.env.APP_NAME,
+    // local APIs
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    API_URL_RENIEC: process.env.API_URL_RENIEC,
+    URL_API_LOCAL: process.env.APP_URL_API_LOCAL,
+    // production APIs
+    URL_API_PRODUCTION: process.env.APP_URL_API_PRODUCTION,
+    //development APIs
+  },
+}
 
-export default nextConfig;
+export default nextConfig
