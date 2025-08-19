6dc4bda7cbc965c9bb3e8315caa1edf8032a8dcd
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-19 08:05:51 -0500

    Feat: fixed data

--- src/modules/dashboard/pages/person/person-filters.tsx
+++ src/modules/dashboard/pages/person/person-filters.tsx
@@ -1,3 +1,14 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export const PersonFilters = () => {
-  return <div>person-filters</div>
+  return (
+    <>
+      <Label className="block mb-2">Filtrar Personas</Label>
+      <Input
+        placeholder="Buscar por nombre, apellido o documento"
+        className="w-full mb-4"
+      />
+    </>
+  )
 }
