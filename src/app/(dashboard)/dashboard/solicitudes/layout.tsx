18855dca61d0f9262a825593d6759d7520fc780e
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-19 14:16:28 -0500

    feat: agregar componente de diseño para gestionar solicitudes y trámites

---
+++ src/app/(dashboard)/dashboard/solicitudes/layout.tsx
@@ -0,0 +1,18 @@
+interface IProps {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: IProps) {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Solicitudes y Trámites</h1>
+        <p className="text-gray-600 mb-6">
+          Aquí puedes gestionar las solicitudes y trámites de tu aplicación.
+        </p>
+      </div>
+
+      {children}
+    </>
+  )
+}
