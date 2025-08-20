7422fef3813fa90f2112afc1351c98a106654fc6
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2025-08-20 07:46:00 -0500

    feat: fixed data

--- src/app/(dashboard)/dashboard/solicitudes/layout.tsx
+++ src/app/(dashboard)/dashboard/solicitudes/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: IProps) {
   return (
     <>
       <div>
-        <h1 className="text-2xl font-bold mb-4">Solicitudes y Trámites</h1>
+        <h1 className="text-2xl font-bold">Solicitudes y Trámites</h1>
         <p className="text-gray-600 mb-6">
           Aquí puedes gestionar las solicitudes y trámites de tu aplicación.
         </p>
